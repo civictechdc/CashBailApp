08dba059a302a0bfc7ccdf1fd1a32b2adba2f98a
kristynf <kristynf@bu.edu>
2020-07-06 12:26:25 -0400

    added PP support

---
+++ src/main/java/com/blacklivesmatter/cashbailbackend/paypal/PayPalController.java
@@ -0,0 +1,18 @@
+package com.blacklivesmatter.cashbailbackend.paypal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PayPalController {
+
+    @Autowired
+    PayPalService service;
+
+    @GetMapping("/donatePaypal")
+    public String donatePaypal(){
+        return "donatePaypal";
+    }
+
+}
