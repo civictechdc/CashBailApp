5956a9a3aed2fd275ad84bdd4423ab0345aff2cb
kristynf <kristynf@bu.edu>
2020-07-06 21:09:03 -0400

    fixed Paypal config class application properties still a bust

--- src/main/java/com/blacklivesmatter/cashbailbackend/paypal/PaypalConfig.java
+++ src/main/java/com/blacklivesmatter/cashbailbackend/paypal/PaypalConfig.java
@@ -3,7 +3,7 @@ package com.blacklivesmatter.cashbailbackend.paypal;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
